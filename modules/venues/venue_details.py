3c10f1e9564549cdde415746bcb4de450af47723
Leonardo Luarte González <leonardo@luarte.net>
2012-08-08 23:34:10 -0400

    Montage and Venue View from public end. Google API now geocodes when possible.

---
+++ modules/venues/venue_details.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+from lib.imports import *
+
+
+class ViewVenue(llhandler.LLGAEHandler):
+	def base_directory(self):
+		return os.path.dirname(__file__)
+	
+	def transitional_get(self,args):
+		self.internal_get(venue_code = args[0])
+
+	def internal_get(self,venue_code):
+		logging.debug("Looking for " + venue_code)
+		venue = STVenue.all().filter('slug =',venue_code).get()
+		self.render('view_venue',template_values={'venue':venue})
