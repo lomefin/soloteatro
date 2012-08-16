fc02c64510047950222252d779105f9e27267ba7
Leonardo Luarte González <leonardo@luarte.net>
2012-08-15 20:21:36 -0400

    Calendar updated, dynamic dates

--- modules/default/index.py
+++ modules/default/index.py
@@ -23,9 +23,26 @@ class DefaultHandler(STHandler):
 	
 	def internal_get(self):
 		
+		calendar_start_date = datetime.date.today() - datetime.timedelta(days = datetime.date.today().isoweekday()-1)
+		calendar_end_date = calendar_start_date + datetime.timedelta(weeks=4)
+		calendar_end_date = calendar_end_date + datetime.timedelta(days = 7 - calendar_end_date.isoweekday())
+		
+		current_date = calendar_start_date
+		month_weeks = []
+		current_month = datetime.date.today().month
+		while(current_date <= calendar_end_date):
+			week = []
+			for i in range(7):
+				current_day = {'day':current_date.day,'current_month':current_month == current_date.month}
+				current_date += datetime.timedelta(days=1)
+				week.append(current_day)
+			month_weeks.append(week)
+
+
 		rand_seasons = db.GqlQuery("SELECT * FROM STSeason WHERE status = 'Open' LIMIT 3")
 		self.set("rand_seasons",rand_seasons)
-		self.render('index',template_values={})
+		self.set("calendar_weeks",month_weeks)
+		self.render('index')
 
 def main():
   LLApp([('/', DefaultHandler)])
